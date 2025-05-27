6f27657e23f0f9ebdd561edfb059181241c847ea
d-klokov <biboran@DESKTOP-2K9L246.>
2025-05-27 19:47:34 +0300

    init: create empty Spring Boot project

---
+++ backend/src/test/java/ru/klokov/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.klokov.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
