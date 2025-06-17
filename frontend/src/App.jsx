22dc4e4344505fb601c04b39595c37d1eabd6496
d-klokov <biboran@DESKTOP-2K9L246.>
2025-06-17 20:22:58 +0300

    feat(frontend): create empty React project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <h1>Hello!</h1>
+    </BrowserRouter>
+  );
+};
+
+export default App;
